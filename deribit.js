552b58daa1549b910d181c42d52cc6ab8d949d7f
Your Name <you@example.com>
2019-01-24 13:55:57 -0400

    test

--- deribit.js
+++ deribit.js
@@ -5,10 +5,10 @@ var request = require("request")
 var bodyParser = require('body-parser')
 app.set('view engine', 'ejs');
 const ccxt = require ('ccxt')
-let exchange = new ccxt.deribit ({  'apiKey': '', 'secret':'' })
+let exchange = new ccxt.deribit ({  'apiKey': 'HYhnLyH9qEvs', 'secret':'YC5OQQH7ECTQTORNALOPSVSPMSFXYWC7YC5OQQH7ECTQTORNALOPSVSPMSFXYWC7' })
 exchange.urls['api'] = exchange.urls['test'];
 app.listen(process.env.PORT || 8080, function() {});
-var restClient = new RestClient('','', 'https://test.deribit.com');
+var restClient = new RestClient('HYhnLyH9qEvs','YC5OQQH7ECTQTORNALOPSVSPMSFXYWC7', 'https://test.deribit.com');
 var startBtc;
 var btcNow;
 var tw = require( './trendyways.min.js')
@@ -20,12 +20,32 @@ var sheet;
 var count = 0;
 var gogo = true;
 var gogoFour = 0;
+var doc = new GoogleSpreadsheet('1pN7RECRznPYKGgpyJdkfTacEX-OxjQyo9YyDLhIRB5M');
 app.get('/update', (req, res) => {
 
 	doPost(req, res)
 
 })
+async.series([
+    function setAuth(step) {
+        var creds = require('./googlesheets.json');
 
+        doc.useServiceAccountAuth(creds, step);
+    },
+    function getInfoAndWorksheets(step) {
+        doc
+            .getInfo(function (err, info) {
+                console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
+                sheet = info.worksheets[0];
+                console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
+                step();
+            });
+    },
+    function workingWithRows(step) {
+
+    }
+    ]
+);
 app.get('/', (req, res) => {
 	doPost(req, res)
 
@@ -302,7 +322,7 @@ for (var o in result[a]){
 		}
 	});
 if (go){
-	tar = tar + ((equity * ha) / 128);
+	tar = tar + ((btcNow * ha) / 128);
 	if (gogoFour < 4){
 		gogoFour++;
 		restClient.sell('BTC-PERPETUAL', tar, ha).then((result) => {
