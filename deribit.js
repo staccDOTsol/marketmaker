b7974ded8c8a24061c2f1359618a52035cf04857
Your Name <you@example.com>
2019-01-24 11:38:45 -0400

    test

--- deribit.js
+++ deribit.js
@@ -6,8 +6,7 @@ var bodyParser = require('body-parser')
 app.set('view engine', 'ejs');
 
 app.listen(process.env.PORT || 8080, function() {});
-var restClient = new RestClient('','', 'https://test.deribit.com');var startBtc;
-var btcNow;
+var restClient = new RestClient('HYhnLyH9qEvs','YC5OQQH7ECTQTORNALOPSVSPMSFXYWC7', 'https://test.deribit.com');var btcNow;
 var tw = require( './trendyways.min.js')
 
 var GoogleSpreadsheet = require('google-spreadsheet');
@@ -15,12 +14,32 @@ var async = require('async');
 var sheet;
 var count = 0;
 var gogo = true;
-
+var doc = new GoogleSpreadsheet('1pN7RECRznPYKGgpyJdkfTacEX-OxjQyo9YyDLhIRB5M');
 app.get('/update', (req, res) => {
 
 	doPost(req, res)
 
 })
+async.series([
+    function setAuth(step) {
+        var creds = require('./googlesheets.json');
+
+        doc.useServiceAccountAuth(creds, step);
+    },
+    function getInfoAndWorksheets(step) {
+        doc
+            .getInfo(function (err, info) {
+                console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
+                sheet = info.worksheets[0];
+                console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
+                step();
+            });
+    },
+    function workingWithRows(step) {
+
+    }
+    ]
+);
 app.get('/', (req, res) => {
 	doPost(req, res)
 
@@ -266,24 +285,28 @@ setInterval(function(){
 					});
 			}
 		}
-		if (result[r][a].size > ((tar * 3 )) || result[r][a].size < (-1 * (tar * 3) )){
+		if (result[r][a].size > ((tar * 3 )						) || result[r][a].size < (-1 * (tar * 3) )){
 
 					liq = 'double outter bounds'
 				var s = result[r][a].size;
 				console.log('20000')
 			if (result[r][a].direction == 'sell'){
-				console.log('sellsell')
-		restClient.buy('BTC-PERPETUAL',  -1 *Math.floor(s/2), lb + 10).then((result) => {
+				console.log('buybuy')
+				restClient.cancelall().then((result) => {
+		restClient.buy('BTC-PERPETUAL',  -1 *Math.floor(s/2), lb).then((result) => {
 			console.log(result);
+					});
 			console.log(result);
 					});
 			} else {
 				console.log('sellsell')
-		restClient.sell('BTC-PERPETUAL', Math.floor(s/2), ha - 10 ).then((result) => {
+				restClient.cancelall().then((result) => {
+		restClient.sell('BTC-PERPETUAL', Math.floor(s/2), ha).then((result) => {
 			console.log(result);
-	});
+					});
+});
 			}
-		}
+		}	
 	}
 		}
 	}
