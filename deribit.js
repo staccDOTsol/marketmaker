4f12a8f542360484ff02bf5ac9ae4b50bd6813ba
Your Name <you@example.com>
2019-01-24 11:21:06 -0400

    test

--- deribit.js
+++ deribit.js
@@ -6,7 +6,8 @@ var bodyParser = require('body-parser')
 app.set('view engine', 'ejs');
 
 app.listen(process.env.PORT || 8080, function() {});
-var restClient = new RestClient('HYhnLyH9qEvs','YC5OQQH7ECTQTORNALOPSVSPMSFXYWC7', 'https://test.deribit.com');var btcNow;
+var restClient = new RestClient('','', 'https://test.deribit.com');var startBtc;
+var btcNow;
 var tw = require( './trendyways.min.js')
 
 var GoogleSpreadsheet = require('google-spreadsheet');
@@ -14,27 +15,7 @@ var async = require('async');
 var sheet;
 var count = 0;
 var gogo = true;
-var doc = new GoogleSpreadsheet('1pN7RECRznPYKGgpyJdkfTacEX-OxjQyo9YyDLhIRB5M');
-async.series([
-    function setAuth(step) {
-        var creds = require('./googlesheets.json');
 
-        doc.useServiceAccountAuth(creds, step);
-    },
-    function getInfoAndWorksheets(step) {
-        doc
-            .getInfo(function (err, info) {
-                console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
-                sheet = info.worksheets[0];
-                console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
-                step();
-            });
-    },
-    function workingWithRows(step) {
-
-    }
-    ]
-);
 app.get('/update', (req, res) => {
 
 	doPost(req, res)
