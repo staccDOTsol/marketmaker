ee26c125e52077d461d3d9b2d89cd00193fa6070
Your Name <you@example.com>
2019-01-24 12:41:33 -0400

    test

--- deribit.js
+++ deribit.js
@@ -6,8 +6,28 @@ var bodyParser = require('body-parser')
 app.set('view engine', 'ejs');
 
 app.listen(process.env.PORT || 8080, function() {});
-var restClient = new RestClient('','', 'https://test.deribit.com');
-var startBtc;
+var restClient = new RestClient('HYhnLyH9qEvs','YC5OQQH7ECTQTORNALOPSVSPMSFXYWC7', 'https://test.deribit.com');
+var doc = new GoogleSpreadsheet('1pN7RECRznPYKGgpyJdkfTacEX-OxjQyo9YyDLhIRB5M');
+async.series([
+    function setAuth(step) {
+        var creds = require('./googlesheets.json');
+
+        doc.useServiceAccountAuth(creds, step);
+    },
+    function getInfoAndWorksheets(step) {
+        doc
+            .getInfo(function (err, info) {
+                console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
+                sheet = info.worksheets[0];
+                console.log('sheet 1: ' + sheet.title + ' ' + sheet.rowCount + 'x' + sheet.colCount);
+                step();
+            });
+    },
+    function workingWithRows(step) {
+
+    }
+    ]
+);var startBtc;
 var btcNow;
 var tw = require( './trendyways.min.js')
 
@@ -194,10 +214,10 @@ var lb = 0;
 var has = []
 var lbs = []
 var tar;
-setTimeout(function(){
+setInterval(function(){
 
 tar = (btcNow * ha) / 4;
-})
+}, 60000)
 var done3x = false;
 setInterval(function(){
 	console.log('interval')
